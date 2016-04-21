3dd721731b872375b631801739ff32c7e96eecdd
fusion809 <brentonhorne77@gmail.com>
2016-04-22 07:58:09 +1000

    Updating

---
+++ index.js
@@ -0,0 +1,28 @@
+var fs   = require('fs'),
+  path = require('path'),
+  marked = require('marked');
+
+/**
+ * Include markdown tag
+ *
+ * Syntax:
+ *   {% include_markdown path/to/file %}
+ */
+
+var mdTagFun= function(args, content){
+	var render = hexo.render,
+	  	sourceDir = hexo.source_dir,
+		arg = args.join(' ');
+
+	arg += ".md";
+	var local = path.join(sourceDir, arg);
+
+	// Exit if the source file doesn't exist
+	if (!fs.existsSync(local)) return local;
+
+	var data = {};
+	data.content = fs.readFileSync(local, 'utf8');
+
+	return marked(data.content)
+};
+hexo.extend.tag.register('include_markdown', mdTagFun);
